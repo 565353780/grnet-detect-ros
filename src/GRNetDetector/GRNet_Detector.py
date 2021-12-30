42bd470be604cc9a7f9f3ee08b6cad7d3f5e6d01
565353780 <565353780@qq.com>
2021-12-30 14:14:34 +0800

    add source files

---
+++ src/GRNetDetector/GRNet_Detector.py
@@ -0,0 +1,186 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import os
+from pprint import pprint
+import numpy as np
+import open3d as o3d
+import torch
+
+from GRNetDetector.config import cfg
+from GRNetDetector.utils.helpers import var_or_cuda
+import GRNetDetector.utils.io
+from GRNetDetector.models.grnet import GRNet
+
+
+class GRNet_Detector:
+    '''
+    Init GRNet config and set torch env param
+    Need to load model after create this class
+    '''
+    def __init__(self) -> None:
+        os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+        torch.backends.cudnn.benchmark = True
+        torch.cuda.set_device(0)
+
+        self.n_points = 2048
+
+        print('Use config:')
+        pprint(cfg)
+
+        self.grnet = GRNet(cfg)
+
+        if torch.cuda.is_available():
+            self.grnet = torch.nn.DataParallel(self.grnet).cuda()
+
+    '''
+    Input:
+        model_path : .pth model path
+    Return:
+        load_success : whether load model success
+    '''
+    def load_model(self, model_path):
+        if not os.path.exists(model_path):
+            print("GRNetDetector::load_model : model doesn't exists!")
+            return False
+
+        print('Recovering from %s ...' % (model_path))
+        checkpoint = torch.load(model_path)
+        self.grnet.load_state_dict(checkpoint['grnet'])
+
+        self.grnet.eval()
+        return True
+
+    '''
+    For KITTI Dataset -> "/home/chli/github/GRNet/frame_0_car_0.txt"
+
+    Input:
+        bbox_file : .txt bbox file path
+    Return:
+        bbox : np.array((8, 3), dtype=np.float32)
+    '''
+    def get_bbox(self, bbox_file):
+        bbox_file_info = []
+        with open(bbox_file, "r") as f:
+            bbox_file_info = f.readlines()
+
+        bbox = []
+
+        for info in bbox_file_info:
+            info_split = info.split("\n")[0].split(" ")
+            temp_index = []
+            for temp_info in info_split:
+                temp_index.append(float(temp_info))
+            bbox.append(temp_index)
+
+        [x_min, y_min, z_min] = bbox[0]
+        [x_max, y_max, z_max] = bbox[0]
+
+        for point in bbox:
+            x_min = min(x_min, point[0])
+            y_min = min(y_min, point[1])
+            z_min = min(z_min, point[2])
+
+            x_max = max(x_max, point[0])
+            y_max = max(y_max, point[1])
+            z_max = max(z_max, point[2])
+
+        x_middle = (x_min + x_max) / 2.0
+        y_middle = (y_min + y_max) / 2.0
+        z_middle = (z_min + z_max) / 2.0
+
+        for point in bbox:
+            point[0] -= x_middle
+            point[1] -= y_middle
+            point[2] -= z_middle
+
+        bbox = np.asarray([bbox], dtype=np.float32)
+
+        return bbox
+
+    '''
+    Input:
+        pointcloud : np.array((<=2048, 3), dtype=np.float32)
+    Return:
+        pointcloud_result : np.array((16384, 3), dtype=np.float32)
+    '''
+    def detect(self, pointcloud):
+        k = ['partial_cloud', 'bounding_box']
+        v = [[], []]
+
+        [x_min, y_min, z_min] = pointcloud[0]
+        [x_max, y_max, z_max] = pointcloud[0]
+        for point in pointcloud:
+            x_min = min(x_min, point[0])
+            y_min = min(y_min, point[1])
+            z_min = min(z_min, point[2])
+
+            x_max = max(x_max, point[0])
+            y_max = max(y_max, point[1])
+            z_max = max(z_max, point[2])
+
+        x_middle = (x_min + x_max) / 2.0
+        y_middle = (y_min + y_max) / 2.0
+        z_middle = (z_min + z_max) / 2.0
+
+        transform = np.asarray([x_middle, y_middle, z_middle])
+
+        pcl_np = pointcloud - transform
+
+        if pcl_np.shape[0] < self.n_points:
+            zeros = np.zeros((self.n_points - pcl_np.shape[0], 3))
+            pcl_np = np.concatenate([pcl_np, zeros])
+
+        pcl_np = np.asarray([pcl_np], dtype=np.float32)
+        v[0] = torch.from_numpy(pcl_np)
+
+        #bbox = self.get_bbox("/home/chli/github/GRNet/frame_0_car_0.txt")
+        #v[1] = torch.from_numpy(bbox)
+
+        data = {}
+
+        data[k[0]] = var_or_cuda(v[0])
+        #data[k[1]] = var_or_cuda(v[1])
+
+        with torch.no_grad():
+            sparse_ptcloud, dense_ptcloud = self.grnet(data)
+
+            pointcloud_result = dense_ptcloud.squeeze().cpu().numpy() + transform
+            return pointcloud_result
+
+    '''
+    Input:
+        pcd_file_path : .pcd file path
+    Return:
+        pointcloud_result : np.array((16384, 3), dtype=np.float32)
+    '''
+    def detect_pcd_file(self, pcd_file_path):
+        pcl = o3d.io.read_point_cloud(pcd_file_path)
+
+        nparray = np.asarray(pcl.points, dtype=np.float32)
+
+        return self.detect(nparray)
+    
+if __name__ == '__main__':
+    model_path = "/home/chli/GRNet/GRNet-ShapeNet.pth"
+    pcd_file_path = \
+        "/home/chli/GRNet/" + \
+        "shapenet-20210607T072352Z-001/shapenet/" + \
+        "test/partial/02691156/" + \
+        "e431f79ac9f0266bca677733d59db4df.pcd"
+
+    grnet_detector = GRNet_Detector()
+
+    grnet_detector.load_model(model_path)
+
+    pointcloud_result = grnet_detector.detect_pcd_file(pcd_file_path)
+
+    output_folder = "/home/chli/GRNet/output/benchmark/02691156"
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    output_file_path = output_folder + '/test_out.h5'
+    utils.io.IO.put(output_file_path, pointcloud_result)
+
+    print('Test Output File = %s' % (output_file_path))
+
