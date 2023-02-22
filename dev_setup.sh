e4ef4a085323433c5d49822126d3d4d077fa9638
chLi <565353780@qq.com>
2023-02-22 17:55:26 +0800

    update setup bash files

--- dev_setup.sh
+++ dev_setup.sh
@@ -4,7 +4,7 @@ pip3 install argparse easydict h5py matplotlib numpy opencv-python pyexr scipy \
 pip3 install tensorboardX==1.2
 
 # pip3 install -U tensorflow-gpu==2.5.0 keras-nightly==2.5.0.dev2021032900 keras==2.4.3
-pip3 install torch torchvision torchaudio \
+pip3 install -U torch torchvision torchaudio \
   --extra-index-url https://download.pytorch.org/whl/cu113
 
 python -m pip install 'git+https://github.com/facebookresearch/detectron2.git'
--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@ pip3 install argparse easydict h5py matplotlib numpy opencv-python pyexr scipy \
 pip3 install tensorboardX==1.2
 
 # pip3 install -U tensorflow-gpu==2.5.0 keras-nightly==2.5.0.dev2021032900 keras==2.4.3
-pip3 install torch torchvision torchaudio \
-  --extra-index-url https://download.pytorch.org/whl/cu113
+pip3 install -U torch torchvision torchaudio \
+  --extra-index-url https://download.pytorch.org/whl/cu116
 
 python -m pip install 'git+https://github.com/facebookresearch/detectron2.git'
 
